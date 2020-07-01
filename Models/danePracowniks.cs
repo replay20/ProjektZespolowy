48f26ffc66bf8d5da6a031762dc629f40d669595
replay20 <taglador@gmail.com>
2020-07-01 13:09:49 +0200

    update

---
+++ Models/danePracowniks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcdanePracowniks.Models
+{
+    public class danePracowniks
+    {
+        public int Id { get; set; }
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public string stanowisko { get; set; }
+        public string uprawnienia { get; set; }
+    }
+}
